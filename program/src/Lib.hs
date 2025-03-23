46a868699cca81ff15fdadedf8bd1464811d5cb9
Alexander <aleksgmr@gmail.com>
2025-03-23 21:15:43 +0300

    new types and functions: base

--- program/src/Lib.hs
+++ program/src/Lib.hs
@@ -4,3 +4,18 @@ module Lib
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+newtype Epsilon = Epsilon Char
+newtype State = State Char
+newtype Terminal = Terminal Char | Epsilon
+
+newtype Transition = Transition (State, Terminal, State)
+newtype Grammar = Grammar [Transition]
+newtype Automaton = Automaton [String]
+
+
+parseGram :: String -> Grammar
+
+normalizeGram :: Grammar -> Grammar
+
+buildAuto :: Grammar -> Automaton
